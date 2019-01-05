d1bc095d7c573104d1807018acc23de80bab7cc8
Filipe Deschamps <filiped@gmail.com>
2019-01-05 16:22:13 -0200

    feat: add debug mode toggle

--- fire.js
+++ fire.js
@@ -1,6 +1,7 @@
 const firePixelsArray = []
-const fireWidth = 60
-const fireHeight = 40
+let fireWidth = 60
+let fireHeight = 40
+let debug = false
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
 
@@ -48,8 +49,6 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
 }
 
 function renderFire() {
-  const debug = false
-
   let html = '<table cellpadding=0 cellspacing=0>'
 
   for (let row = 0; row < fireHeight; row++) {
@@ -58,15 +57,15 @@ function renderFire() {
     for (let column = 0; column < fireWidth; column++) {
       const pixelIndex = column + ( fireWidth * row )
       const fireIntensity = firePixelsArray[pixelIndex]
+      const color = fireColorsPalette[fireIntensity]
+      const colorString = `${color.r},${color.g},${color.b}`
 
       if (debug === true) {
         html += '<td>'
         html += `<div class="pixel-index">${pixelIndex}</div>`
-        html += fireIntensity
+        html += `<div style="color: rgb(${colorString})">${fireIntensity}</div>`
         html += '</td>'
       } else {
-        const color = fireColorsPalette[fireIntensity]
-        const colorString = `${color.r},${color.g},${color.b}`
         html += `<td class="pixel" style="background-color: rgb(${colorString})">`
         html += '</td>'
       }
@@ -128,6 +127,21 @@ function decreaseFireSource() {
       firePixelsArray[pixelIndex] = newFireIntensity
     }
   }
+}
+
+function toggleDebugMode() {
+  if (debug === false) {
+    fireWidth = 25
+    fireHeight = 17
+    debug = true
+  } else {
+    fireWidth = 60
+    fireHeight = 40
+    debug = false
+  }
+
+  createFireDataStructure()
+  createFireSource()
 }
 
 start()
