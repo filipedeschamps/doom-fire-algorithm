96ef3a53cd15bc2ff5d32e0974956c144a42b232
leocavalcante <lc@leocavalcante.com>
2019-01-05 17:50:43 -0200

    refactor: extract current dom table engine

---
+++ engines/dom-table.js
@@ -0,0 +1,30 @@
+function domTable() {
+  let html = '<table cellpadding=0 cellspacing=0>'
+
+  for (let row = 0; row < fireHeight; row++) {
+    html += '<tr>'
+
+    for (let column = 0; column < fireWidth; column++) {
+      const pixelIndex = column + ( fireWidth * row )
+      const fireIntensity = firePixelsArray[pixelIndex]
+      const color = fireColorsPalette[fireIntensity]
+      const colorString = `${color.r},${color.g},${color.b}`
+
+      if (debug === true) {
+        html += '<td>'
+        html += `<div class="pixel-index">${pixelIndex}</div>`
+        html += `<div style="color: rgb(${colorString})">${fireIntensity}</div>`
+        html += '</td>'
+      } else {
+        html += `<td class="pixel" style="background-color: rgb(${colorString})">`
+        html += '</td>'
+      }
+    }
+
+    html += '</tr>'
+  }
+
+  html += '</table>'
+
+  document.querySelector('#fireCanvas').innerHTML = html
+}
--- fire.js
+++ fire.js
@@ -49,34 +49,7 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
 }
 
 function renderFire() {
-  let html = '<table cellpadding=0 cellspacing=0>'
-
-  for (let row = 0; row < fireHeight; row++) {
-    html += '<tr>'
-
-    for (let column = 0; column < fireWidth; column++) {
-      const pixelIndex = column + ( fireWidth * row )
-      const fireIntensity = firePixelsArray[pixelIndex]
-      const color = fireColorsPalette[fireIntensity]
-      const colorString = `${color.r},${color.g},${color.b}`
-
-      if (debug === true) {
-        html += '<td>'
-        html += `<div class="pixel-index">${pixelIndex}</div>`
-        html += `<div style="color: rgb(${colorString})">${fireIntensity}</div>`
-        html += '</td>'
-      } else {
-        html += `<td class="pixel" style="background-color: rgb(${colorString})">`
-        html += '</td>'
-      }
-    }
-
-    html += '</tr>'
-  }
-
-  html += '</table>'
-
-  document.querySelector('#fireCanvas').innerHTML = html  
+  domTable()
 }
 
 function createFireSource() {
