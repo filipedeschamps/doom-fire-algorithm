37dd255cc368c20eb68f00a13b8e76972598c43f
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2019-01-17 14:40:25 -0200

    Render using canvas image + Change wind direction

--- fire.js
+++ fire.js
@@ -1,9 +1,13 @@
 const firePixelsArray = []
 let fireWidth = 60
 let fireHeight = 40
-let debug = false
+let windDirection = 'right'
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
-
+const canvas = document.getElementById('fireCanvas')
+canvas.width = fireWidth
+canvas.height = fireHeight
+const context = canvas.getContext('2d')
+const image = context.createImageData(fireWidth, fireHeight)
 
 function start() {
   createFireDataStructure()
@@ -32,6 +36,10 @@ function calculateFirePropagation() {
   renderFire()
 }
 
+function calculateWindPixelIndex(currentPixelIndex, windPixel) {
+  return (windDirection === 'right' ? currentPixelIndex - windPixel : currentPixelIndex + windPixel)
+}
+
 function updateFireIntensityPerPixel(currentPixelIndex) {
   const belowPixelIndex = currentPixelIndex + fireWidth
 
@@ -40,43 +48,26 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
     return
   }
 
-  const decay = Math.floor(Math.random() * 3)
+  const windPixel = Math.floor(Math.random() * 3)
   const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
-  const newFireIntensity =
-    belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
+  const newFireIntensity = belowPixelFireIntensity - windPixel >= 0 ? belowPixelFireIntensity - windPixel : 0
+  const windPixelIndex = calculateWindPixelIndex(currentPixelIndex, windPixel)
 
-  firePixelsArray[currentPixelIndex - decay] = newFireIntensity
+  firePixelsArray[windPixelIndex] = newFireIntensity
 }
 
 function renderFire() {
-  let html = '<table cellpadding=0 cellspacing=0>'
+  for (let pixelIndex = 0; pixelIndex < firePixelsArray.length; pixelIndex++) {
+    const fireIntensity = firePixelsArray[pixelIndex]
+    const color = fireColorsPalette[fireIntensity]
 
-  for (let row = 0; row < fireHeight; row++) {
-    html += '<tr>'
-
-    for (let column = 0; column < fireWidth; column++) {
-      const pixelIndex = column + ( fireWidth * row )
-      const fireIntensity = firePixelsArray[pixelIndex]
-      const color = fireColorsPalette[fireIntensity]
-      const colorString = `${color.r},${color.g},${color.b}`
-
-      if (debug === true) {
-        html += '<td>'
-        html += `<div class="pixel-index">${pixelIndex}</div>`
-        html += `<div style="color: rgb(${colorString})">${fireIntensity}</div>`
-        html += '</td>'
-      } else {
-        html += `<td class="pixel" style="background-color: rgb(${colorString})">`
-        html += '</td>'
-      }
-    }
-
-    html += '</tr>'
+    image.data[pixelIndex * 4] = color.r
+    image.data[pixelIndex * 4 + 1] = color.g
+    image.data[pixelIndex * 4 + 2] = color.b
+    image.data[pixelIndex * 4 + 3] = 255
   }
-
-  html += '</table>'
-
-  document.querySelector('#fireCanvas').innerHTML = html  
+  
+  context.putImageData(image, 0, 0)
 }
 
 function createFireSource() {
@@ -129,17 +120,8 @@ function decreaseFireSource() {
   }
 }
 
-function toggleDebugMode() {
-  if (debug === false) {
-    fireWidth = 25
-    fireHeight = 17
-    debug = true
-  } else {
-    fireWidth = 60
-    fireHeight = 40
-    debug = false
-  }
-
+function changeWindDirection() {
+  windDirection = (windDirection === 'right' ? 'left' : 'right')
   createFireDataStructure()
   createFireSource()
 }
