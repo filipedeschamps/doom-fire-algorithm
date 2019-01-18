d4571c4551b408010f078887cd912cf82a61873d
leocavalcante <lc@leocavalcante.com>
2019-01-18 21:44:19 -0200

    Add Pixi Rendering

--- playground/render-with-pixijs/fire.js
+++ playground/render-with-pixijs/fire.js
@@ -2,13 +2,17 @@ const firePixelsArray = []
 let fireWidth = 200
 let fireHeight = 80
 const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
-const app = new PIXI.Application(fireWidth, fireHeight)
+
+const pixelSize = 4
+const app = new PIXI.Application(fireWidth * pixelSize, fireHeight * pixelSize)
 const graphics = new PIXI.Graphics()
+const from10To16 = n => n.toString(16).padStart(2, '0')
 
 function start() {
   createFireDataStructure()
   createFireSource()
 
+  app.stage.addChild(graphics)
   document.querySelector('#fireCanvas').appendChild(app.view)
 
   setInterval(calculateFirePropagation, 50)
@@ -51,15 +55,18 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
 }
 
 function renderFire() {
+  graphics.clear()
+
   for (let row = 0; row < fireHeight; row++) {
     for (let column = 0; column < fireWidth; column++) {
       const pixelIndex = column + (fireWidth * row)
       const fireIntensity = firePixelsArray[pixelIndex]
       const color = fireColorsPalette[fireIntensity]
-      const colorString = `${color.r},${color.g},${color.b}`
+      const colorString = `${from10To16(color.r)}${from10To16(color.g)}${from10To16(color.b)}`
 
-      canvasCtx.fillStyle = ` rgb(${colorString})`
-      canvasCtx.fillRect(column * pixelSize, row * pixelSize, pixelSize, pixelSize)
+      graphics.beginFill(parseInt(colorString, 16))
+      graphics.drawRect(column * pixelSize, row * pixelSize, pixelSize, pixelSize)
+      graphics.endFill()
     }
   }
 }
