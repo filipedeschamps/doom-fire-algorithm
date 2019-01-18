1688d70ef602e95602fa43ed1b8c98ca08b3e518
Filipe Deschamps <filiped@gmail.com>
2019-01-18 16:40:45 -0200

    fix: increase canvas size, decrease decay and remove debug mode
    from @leocavalcante experiment

--- playground/render-with-canvas-draw-rect/fire.js
+++ playground/render-with-canvas-draw-rect/fire.js
@@ -1,7 +1,6 @@
 const firePixelsArray = []
-let fireWidth = 60
-let fireHeight = 40
-let debug = false
+let fireWidth = 200
+let fireHeight = 80
 const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
 const canvas = document.createElement('canvas')
 const canvasCtx = canvas.getContext('2d')
@@ -46,7 +45,7 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
     return
   }
 
-  const decay = Math.floor(Math.random() * 3)
+  const decay = Math.floor(Math.random() * 2)
   const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
   const newFireIntensity =
     belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
@@ -116,21 +115,6 @@ function decreaseFireSource() {
       firePixelsArray[pixelIndex] = newFireIntensity
     }
   }
-}
-
-function toggleDebugMode() {
-  if (debug === false) {
-    fireWidth = 25
-    fireHeight = 17
-    debug = true
-  } else {
-    fireWidth = 60
-    fireHeight = 40
-    debug = false
-  }
-
-  createFireDataStructure()
-  createFireSource()
 }
 
 start()
