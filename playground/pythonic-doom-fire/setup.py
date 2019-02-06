8b43bd6a4e354fe4a738f5e93956b0f68c880749
Filipe Alves <contato@filipealves.com.br>
2019-02-06 03:39:01 -0200

    pythonic-doom-fire: add minimal package structure

---
+++ playground/pythonic-doom-fire/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='doom-fire-algorithm-pygame',
+    version='1.0',
+    description='Doom fire algorithm implemented with pygame',
+    author='Filipe Alves',
+    author_email='filipe.alvesdefernando@hotmail.com',
+    packages=['doom-fire-algorithm-pygame'],
+    install_requires=['pygame'],
+)
