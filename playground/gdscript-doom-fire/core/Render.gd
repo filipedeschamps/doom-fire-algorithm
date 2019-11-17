ace38a1a39e8eac428233627dc4925d82c8d6af9
jshlw <JulioHenrique31501234@hotmail.com>
2019-11-17 19:21:53 -0300

    Fixed timer bug

--- playground/gdscript-doom-fire/core/Render.gd
+++ playground/gdscript-doom-fire/core/Render.gd
@@ -3,19 +3,38 @@ extends Sprite
 
 export(Vector2) var size = Vector2(40, 40)
 export(int) var pixel_scale = 10
+export(float) var update_time = 1 / 10.0
 
 const COLORS = [Color.white, Color.black]
+
+var timer = Timer.new()
 
 
 func _ready():
 	centered = false
 	scale = Vector2(pixel_scale, pixel_scale)
 	render()
+	
+	timer.connect("timeout", self, "request_render")
+	timer.wait_time = update_time
+	timer.one_shot = true
+	timer.autostart = true
+	
+	add_child(timer)
+
+
+func request_render():
+	render()
+	timer.wait_time = update_time
+	timer.start()
 
 
 func render():
+	if texture != null:
+		texture = null
+	
 	var img = Image.new()
-	img.create(size.x, size.y, false, Image.FORMAT_RGB8)
+	img.create(size.x, size.y, false, Image.FORMAT_RGBA8)
 	
 	for x in range(img.get_width()):
 		for y in range(img.get_height()):
@@ -29,3 +48,5 @@ func render():
 	tex.flags = 0
 	texture = tex
 	update()
+	
+	img = null
