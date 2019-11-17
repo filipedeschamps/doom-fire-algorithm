cbdcff465c8d63629bc305531ea207872990111a
jshlw <JulioHenrique31501234@hotmail.com>
2019-11-17 19:29:31 -0300

    Add color palette

--- playground/gdscript-doom-fire/core/Render.gd
+++ playground/gdscript-doom-fire/core/Render.gd
@@ -5,7 +5,13 @@ export(Vector2) var size = Vector2(40, 40)
 export(int) var pixel_scale = 10
 export(float) var update_time = 1 / 10.0
 
-const COLORS = [Color.white, Color.black]
+const COLORS = [
+	Color(7, 7, 7), Color( 31,  7,  7), Color( 47, 15,  7), Color( 71, 15,  7), Color( 87, 23,  7), Color(103, 31,  7), Color(119, 31,  7), Color(143, 39,  7),
+	Color(159, 47,  7), Color(175, 63,  7), Color(191, 71,  7), Color(199, 71,  7), Color(223, 79,  7), Color(223, 87,  7), Color(223, 87,  7), Color(215, 95,  7),
+	Color(215, 95,  7), Color(215,103, 15), Color(207,111, 15), Color(207,119, 15), Color(207,127, 15), Color(207,135, 23), Color(199,135, 23), Color(199,143, 23),
+	Color(199,151, 31), Color(191,159, 31), Color(191,159, 31), Color(191,167, 39), Color(191,167, 39), Color(191,175, 47), Color(183,175, 47), Color(183,183, 47),
+	Color(183,183, 55), Color(207,207,111), Color(223,223,159), Color(239,239,199), Color(255,255,255)
+]
 
 var timer = Timer.new()
 
@@ -41,7 +47,8 @@ func render():
 	for x in range(img.get_width()):
 		for y in range(img.get_height()):
 			img.lock()
-			img.set_pixel(x, y, COLORS[target.get_pixel(Vector2(x, y))])
+			var col = COLORS[target.get_pixel(Vector2(x, y))]
+			img.set_pixel(x, y, Color(col.r / 255.0, col.g / 255.0, col.b / 255))
 			img.unlock()
 	
 	var tex = ImageTexture.new()
