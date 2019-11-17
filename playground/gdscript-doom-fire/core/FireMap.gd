e79a6a2ab61477c2115683aae86f62d1cf60b234
jshlw <JulioHenrique31501234@hotmail.com>
2019-11-17 19:38:53 -0300

    Add fill

--- playground/gdscript-doom-fire/core/FireMap.gd
+++ playground/gdscript-doom-fire/core/FireMap.gd
@@ -3,22 +3,41 @@ extends Node
 
 var map = []
 
-var width = 40 setget , get_width
-var height = 40 setget , get_height
+export(int) var width = 40 setget , get_width
+export(int) var height = 40 setget , get_height
+export(int) var decay = 1
+
+
+var timer = Timer.new()
 
 
 func _ready():
+	fill()
+
+
+func fill():
 	for x in range(width):
 		for y in range(height):
 			map.append(0)
 
 
-func generate():
-	pass
+func generate_fire():
+	for x in range(width):
+		for y in range(height):
+			map[get_map_id(Vector2(x, y))]
+
+
+func update_fire_intensity(pixel):
+	randomize()
+	map[get_map_id(Vector2(39, 39))] = randi()%10
+
+
+func get_map_id(vec):
+	return vec.x + (height * vec.y)
 
 
 func get_pixel(vec):
-	return map[vec.x + (height * vec.y)]
+	return map[get_map_id(vec)]
 
 
 func get_size():
